5bb20a3d3eef09d9d403df25366138538a2aa35e
Roger Guldbrandsen <roger@kinbiko.com>
2017-08-26 06:45:02 +0100

    Minor code improvements

--- src/main/java/com/kinbiko/BugsnagDeployMojo.java
+++ src/main/java/com/kinbiko/BugsnagDeployMojo.java
@@ -11,20 +11,26 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.IOException;
 
+/**
+ * Mojo responsible for sending deploy notifications to Bugsnag,
+ * based on the plugin configuration.
+ */
 @Mojo(name = "deploy")
 public class BugsnagDeployMojo extends AbstractMojo {
 
-    private static final String DEFAULT_RELEASE_STAGE = "production";
-
-    @Parameter(required = true, property = "deploy.apiKey")
+    @Parameter(property = "deploy.apiKey", required = true)
     private String apiKey;
 
-    @Parameter(property = "deploy.releaseStage", defaultValue = DEFAULT_RELEASE_STAGE)
+    @Parameter(property = "deploy.releaseStage", defaultValue = "production")
     private String releaseStage;
 
-    @Parameter(property = "appVersion", defaultValue = "${project.version}")
+    @Parameter(property = "deploy.appVersion", defaultValue = "${project.version}")
     private String appVersion;
 
+    /**
+     * If the API key has been properly configured, this method will send a deploy notification
+     * to Bugsnag, otherwise an error message will be displayed.
+     */
     public void execute() throws MojoExecutionException {
         getLog().info("Sending deploy notification to Bugsnag. App version: " + appVersion);
         sendDeployNotification();
@@ -34,24 +40,28 @@ public class BugsnagDeployMojo extends AbstractMojo {
     private void sendDeployNotification() {
         try {
             final HttpResponse<String> res = makeHttpCall();
-            if (res.getStatus() > 399)
-                getLog().error("Failed to send deploy notification: "
-                        + res.getStatusText()
-                        + ". Ensure the API key is valid.");
-            else
-                getLog().info("Deploy notification successful");
+            logResults(res);
         } catch (final UnirestException e) {
             getLog().error("Something went wrong internally with the plugin. Enable debug logging for more information");
             getLog().debug(e);
         }
     }
 
+    private void logResults(final HttpResponse<String> res) {
+        if (res.getStatus() > 400)
+            getLog().error("Failed to send deploy notification: " + res.getStatusText());
+        else if (res.getStatus() == 400)
+            getLog().error("Invalid configuration. Ensure the API key is valid.");
+        else
+            getLog().info("Deploy notification successful");
+    }
+
     private HttpResponse<String> makeHttpCall() throws UnirestException {
         return Unirest.post("https://notify.bugsnag.com/deploy")
-                        .field("apiKey", apiKey)
-                        .field("releaseStage", releaseStage)
-                        .field("appVersion", appVersion)
-                        .asString();
+                .field("apiKey", apiKey)
+                .field("releaseStage", releaseStage)
+                .field("appVersion", appVersion)
+                .asString();
     }
 
     private void shutdown() throws MojoExecutionException {
